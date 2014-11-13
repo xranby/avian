42b020790199e096ae3335fc14494367480dda6c
Matthias Klose <doko@ubuntu.com>
2014-11-13 14:38:33 +0100

    avian (1.1.0-1)
    Thanks!  some rhings:
    
     - I regenerated the .orig.tar.gz, because there seems to be a
       leftover directory build-cmake without having sources. This
       would be rejected by the ftp masters.
    
     - I didn't update OpenJDK 7 in jessie yet. Did you try to build/run these with
       the IcedTea 2.5.3 update? At least Cacao and JamVM fail.
    
     - You still build using GCC 4.7.  I'm trying to get rid off 4.7
       for jessie, see bug #765379. Using the default now, and
       using no lto, because it ICEs.
    
     - removed all debian/*.ex files, same with README.source.
    
    Uploaded for now.
    
      Matthias

--- debian/avian.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the avian package
-#
-0 4	* * *	root	[ -x /usr/bin/avian_maintenance ] && /usr/bin/avian_maintenance
--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(avian):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="avian" command="/usr/bin/avian"
