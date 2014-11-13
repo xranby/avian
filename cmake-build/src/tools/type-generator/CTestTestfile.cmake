5358c3aa33355f097bdb8a47ca1b451c5a9e9368
Xerxes Rånby <xerxes@gudinna.com>
2014-11-13 14:51:38 +0100

    Imported Upstream version 1.1.0

--- cmake-build/src/tools/type-generator/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/dicej/p/avian/src/tools/type-generator
-# Build directory: /home/dicej/p/avian/cmake-build/src/tools/type-generator
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
